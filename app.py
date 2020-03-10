fdf27733d8df537e7d801c5362095e62908da8c0
JT Liner <jtliner925@gmail.com>
2020-03-10 12:35:12 -0500

    formatting to dashboard

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ import dash_html_components as html
 import pandas as pd
 import pickle
 
-app = dash.Dash()
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 #load in data file
 DATA_PATH = '/Users/ChristyLiner/Documents/Corona Virus/COVID_19'
@@ -12,29 +14,71 @@ pickle_in = open(DATA_PATH + '/df_patient.pickle', 'rb')
 df_patient = pickle.load(pickle_in)
     
 colors = {
-    'header_background': '#111111',
-    'text': '#7FDBFF',
-    'graph_background': '#FFFFFF',
-    'page_background': '#FFFFFF'
+    'body_background': '#181A1C',
+    'header_background': '#232629',
+    'text': '#d6d9dc',
+    'graph_background': '#181A1C',
+    'page_background': '#181A1C',
+    'bar_color': '#b30000',
 }
-app.layout = html.Div(style={'backgroundColor': colors['header_background']}, children=[
+app.layout = html.Main(
+    style={
+    'backgroundColor': colors['body_background'],
+    'padding': '10px'
+    },
+    children=[
+        html.Div(style={
+    'backgroundColor': colors['body_background'],
+    
+    }),  
+    
     html.H1(
         children='COVID-19 Coronavirus',
         style={
+            'backgroundColor': colors['header_background'],
             'textAlign': 'center',
-            'color': colors['text']
-        }
+            'color': colors['text'],
+            'font-family': 'Verdana',
+            'box-shadow': '0 1px 3px 0 rgba(0, 0, 0, 2)',
+            # 'margin': '10px',
+            'margin-top': '10px'}  
     ),
-    html.Div(children='Subtitle', style={
+       html.Div(children='Sample sub-heading', style={
         'textAlign': 'center',
-        'color': colors['text']
+        'backgroundColor': colors['header_background'],
+            'color': colors['text'],
+            'font-family': 'Verdana',
+            'font-size': '24px',
+            'margin': 'auto',
+            'box-shadow': '0 1px 3px 0 rgba(0, 0, 0, 2)',
+            'margin-bottom': '10px'
+        
     }),
+
+    # html.Div(children='Brian Kosiadi | Andrei Zholud PhD | Christy Liner MBA', style={
+    #     'textAlign': 'center',
+    #         'color': colors['text'],
+    #         'backgroundColor': colors['header_background'],
+    #         'font-family': 'Verdana',
+    #         'box-shadow': '0 1px 3px 0 rgba(0, 0, 0, 2)',
+    #         'margin-bottom': '10px'
+         
+    # }),
+
+    
 
     dcc.Graph(
         id='Graph1',
+        style={
+            'box-shadow': '0 1px 3px 0 rgba(0, 0, 0, 2)'
+        },
         figure={
             'data': [
-                {'x': df_patient['region'], 'y': df_patient['age'], 'type': 'bar'},
+                {
+                    'x': df_patient['region'], 
+                    'y': df_patient['age'], 
+                    'type': 'bar'
+                    },
             ],
             'layout': {
                 'plot_bgcolor': colors['graph_background'],
@@ -45,8 +89,17 @@ app.layout = html.Div(style={'backgroundColor': colors['header_background']}, ch
             }
         }
     ),
-    
-])
+    html.Footer(children='Brian Kosiadi | Andrei Zholud PhD | Christy Liner MBA', style={
+        'textAlign': 'center',
+            'color': colors['text'],
+            'backgroundColor': colors['header_background'],
+            'font-family': 'Verdana',
+            'box-shadow': '0 1px 3px 0 rgba(0, 0, 0, 2)',
+            'margin-top': '20px',
+            'margin-bottom': '0'
+         
+    })]
+)
 
 
 
