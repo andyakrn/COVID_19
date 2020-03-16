4b61b4334ba5772227846cfd42e60d1186011c33
JT Liner <jtliner925@gmail.com>
2020-03-16 09:47:16 -0500

    changes

--- App files/visualizations.py
+++ App files/visualizations.py
@@ -1,10 +1,4 @@
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
-from colors import colors
-import pandas as pd
-import pickle
-
+from imports import *
 
 # load in data file
 DATA_PATH = '/Users/ChristyLiner/Documents/Corona Virus/COVID_19'
@@ -16,8 +10,7 @@ world_map = dcc.Graph(
     style={
         'margin-right': '5px',
         'box-shadow': '0 1px 3px 0 rgba(0, 0, 0, 2)',
-        'width': '100%',
-    },
+        'width': '100%'},
     figure={
         'data': [
             {'x': df_patient['region'],
@@ -54,9 +47,9 @@ graph1 = dcc.Graph(
             'title': 'Test Title',
             'plot_bgcolor': colors['graph_background'],
             'paper_bgcolor': colors['graph_background'],
-            'font':
-            {'color': colors['text']
-             },
+           
+            'color': colors['text']
+             ,
         }
     },
 )
@@ -79,61 +72,107 @@ graph2 = dcc.Graph(
             'title': 'Test Title',
             'plot_bgcolor': colors['graph_background'],
             'paper_bgcolor': colors['graph_background'],
-            'font':
-            {'color': colors['text']
-             },
+
+            'color': colors['text']
+
         }
     }
 )
-#bubble map with animation 
-#https://plot.ly/python/bubble-maps/
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/2011_february_us_airport_traffic.csv')
-df.head()
+# bubble map with animation
+# https://plot.ly/python/bubble-maps/
 
-df['text'] = df['airport'] + '' + df['city'] + ', ' + df['state'] + '' + 'Arrivals: ' + df['cnt'].astype(str)
+scl = [[0, "rgb(5, 10, 172)"], [0.35, "rgb(40, 60, 190)"], [0.5, "rgb(70, 100, 245)"],
+       [0.6, "rgb(90, 120, 245)"], [0.7, "rgb(106, 137, 247)"], [1, "rgb(220, 220, 220)"]]
 
-scl = [ [0,"rgb(5, 10, 172)"],[0.35,"rgb(40, 60, 190)"],[0.5,"rgb(70, 100, 245)"],\
-    [0.6,"rgb(90, 120, 245)"],[0.7,"rgb(106, 137, 247)"],[1,"rgb(220, 220, 220)"] ]
+DATA_PATH1 = '/Users/ChristyLiner/Documents/Corona Virus/COVID_19'
+pickle_in = open(DATA_PATH1 + '/COVID_Hopkins_df.pickle', 'rb')
+grouped_df = pickle.load(pickle_in)
+
+data = [dict(
+    type='scattergeo',
+    locations=grouped_df['Country/Region'],
+    locationmode='country names',
+    scope='world',
+    size=grouped_df['Confirmed'],
+    # text=df['text'],
+    # mode='markers',
+    # animation_frame=grouped_df['Confirmed'],
+    marker=dict(
+        size=8,
+        opacity=0.8,
+        reversescale=True,
+        autocolorscale=False,
+        # symbol='square',
+        line=dict(
+            width=1,
+            color='rgba(102, 102, 102)'),
+        colorscale=scl,
+        cmin=0,
+        color=grouped_df['Country/Region'],
+        # cmax=df['cnt'].max(),
+        colorbar=dict(
+            title="Incoming flightsFebruary 2011"
+        )
+    ))]
+layout = dict(
+    title='Global COVID-19 Cases<br>(Hover for country names)',
+    colorbar=True,
+    geo=dict(
+        showland=True,
+        landcolor="rgb(250, 250, 250)",
+        subunitcolor="rgb(217, 217, 217)",
+        countrycolor="rgb(217, 217, 217)",
+        countrywidth=0.5,
+        subunitwidth=0.5
+    ),
+)
+fig = dict(data=data, layout=layout)
+
+world_map2 = dcc.Graph(id='Worldmap2', figure=fig)
+
 
 data = [ dict(
         type = 'scattergeo',
-        locationmode = 'USA-states',
-        lon = df['long'],
-        lat = df['lat'],
-        text = df['text'],
+        locationmode = 'country names',
+        locations = grouped_df['Country/Region'],
+        
+      
         mode = 'markers',
         marker = dict(
             size = 8,
             opacity = 0.8,
             reversescale = True,
             autocolorscale = False,
-            symbol = 'square',
+            symbol = 'circle',
             line = dict(
                 width=1,
                 color='rgba(102, 102, 102)'
             ),
             colorscale = scl,
             cmin = 0,
-            color = df['cnt'],
-            cmax = df['cnt'].max(),
+            color = grouped_df['Confirmed'],
+            cmax = grouped_df['Confirmed'].max(),
             colorbar=dict(
-                title="Incoming flightsFebruary 2011"
+                title="Test"
             )
         ))]
+
 layout = dict(
-        title = 'Most trafficked US airports<br>(Hover for airport names)',
+        title = 'Test Title<br>(Hover for Country Names)',
         colorbar = True,
         geo = dict(
-            scope='usa',
-            projection=dict( type='albers usa' ),
+            scope='world',
+            projection=dict(type='natural earth'),
             showland = True,
             landcolor = "rgb(250, 250, 250)",
             subunitcolor = "rgb(217, 217, 217)",
             countrycolor = "rgb(217, 217, 217)",
             countrywidth = 0.5,
             subunitwidth = 0.5
-        ),
-    )
-fig = dict( data=data, layout=layout )    
+        ))
 
-world_map2 = dcc.Graph(id = 'Worldmap2', figure = fig)
+    # 'plot_bgcolor': colors['graph_background'],
+    # 'paper_bgcolor': colors['graph_background'],
+    # 'color': colors['text']
+
+
