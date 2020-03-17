c8cc7b87a14610270c6123877ec350075d5d9f9c
JT Liner <jtliner925@gmail.com>
2020-03-16 21:55:13 -0500

    commit changes

--- App files/data.py
+++ App files/data.py
@@ -5,3 +5,5 @@ import pandas as pd
 DATA_PATH1 = '/Users/ChristyLiner/Documents/Corona Virus/COVID_19'
 pickle_in = open(DATA_PATH1 + '/COVID_Hopkins_df.pickle', 'rb')
 grouped_df = pickle.load(pickle_in)
+
+#border
