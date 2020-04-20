adbf49aff0039671a1f01c8c4d48b62a4efaf823
Christy Liner <christyliner@Christys-iMac.local>
2020-04-19 19:26:19 -0500

    Updated Data

--- auto_script.sh
+++ auto_script.sh
@@ -1,5 +1,5 @@
-python CSSE_US_data.py
-python Data/Hopkins_data.py
+python /Users/christyliner/Documents/COVID_19/CSSE_US_data.py
+python /Users/christyliner/Documents/COVID_19/Data/Hopkins_data.py
 
 git add .
 git commit -m 'Updated Data'
